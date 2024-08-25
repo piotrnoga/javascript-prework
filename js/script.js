78f3479569b8e0ddbb01058403e24c815f49f0ef
Piotr Noga <piotrnoga.dev@gmail.com>
2024-08-25 16:03:59 +0200

    Add if...else instructions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
-let computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+let computerMove = 'nieznany ruch';
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2) {
+    computerMove = 'papier'
+} else {
+    computerMove = 'nożyce'
+}
+printMessage('Ruch komputera to: ' + computerMove);
 
-let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-let randomNumber = Math.floor(Math.random() * 9 + 11);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+    console.log('Gracz wpisał: ' + playerInput);
+    let playerMove = 'nieznany ruch';
+    if(playerInput == '1'){
+      playerMove = 'kamień';
+    } else if(playerInput == '2') {
+        playerMove = 'papier'
+    } else if(playerInput == '3'){
+        playerMove = 'nożyce'
+    }
+    printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == playerMove){
+    printMessage('Remis!');
+} else if(playerMove == 'nieznany ruch') {
+    printMessage('Wpisałeś złą wartość!')
+} else {
+    printMessage('Komputer wygrywa!')
+}
+
+
+
+// let computerMove = 'kamień';
+// printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+// let playerMove = 'papier';
+// printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+// let randomNumber = Math.floor(Math.random() * 9 + 11);
+// printMessage('Wylosowana liczba to: ' + randomNumber);
