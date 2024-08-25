a1fd54f3d00ac7851d754b29c901ac7f445e3df7
Piotr Noga <piotrnoga.dev@gmail.com>
2024-08-25 18:55:31 +0200

    Add css, blocked screen after win and cup for the winner

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,21 @@ function printMessage(msg){
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
 }
+
 function clearMessages(){
     document.getElementById('messages').innerHTML = '';
+}
+
+function printResultTable(result){
+    let div = document.createElement('div');
+    div.innerHTML = result;
+    document.getElementById('result').appendChild(div);
+}
+
+function printResultTable(result) {
+    document.getElementById('result').innerHTML = result;
+}
+
+function printWinner(winner) {
+    document.getElementById('winner').innerHTML = winner;
 }
--- js/script.js
+++ js/script.js
@@ -20,22 +20,88 @@ function playGame(argPlayerMove) {
     // FUNCTION - WHAT IS THE RESULT?
 
     function displayResult(argComputerMove, argPlayerMove) {
-        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        if (argComputerMove == 'kamień') {
+            document.getElementById('computer-paper').classList.remove('choice');
+            document.getElementById('computer-scissors').classList.remove('choice');
+            
+            document.getElementById('computer-rock').classList.add('choice');
+
+        } else if (argComputerMove == 'papier') {
+            document.getElementById('computer-rock').classList.remove('choice');
+            document.getElementById('computer-scissors').classList.remove('choice');
+            
+            document.getElementById('computer-paper').classList.add('choice');
+        } else if (argComputerMove == 'nożyce') {
+            document.getElementById('computer-paper').classList.remove('choice');
+            document.getElementById('computer-rock').classList.remove('choice');
+            
+            document.getElementById('computer-scissors').classList.add('choice');
+        }
+
+        printMessage('Komputer zagrał ' + argComputerMove + ', a Ty ' + argPlayerMove);
         
         if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-            printMessage('Ty wygrywasz!');
+            printMessage('Ty wygrywasz tę rundę');
+            playerPoints += 1;
         } else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-            printMessage('Ty wygrywasz!');
+            printMessage('Ty wygrywasz tę rundę');
+            playerPoints += 1;
         } else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-            printMessage('Ty wygrywasz!');
+            printMessage('Ty wygrywasz tę rundę!');
+            playerPoints += 1;
         } else if(argComputerMove == argPlayerMove){
             printMessage('Remis!');
         } else if(argPlayerMove == 'nieznany ruch') {
             printMessage('Wpisałeś złą wartość!')
         } else {
-            printMessage('Komputer wygrywa!')
+            printMessage('Komputer wygry tę rundę!')
+            computerPoints += 1;
         }
     }
+
+    // FUNCTION WHO WON THE GAME
+
+    function whoWon(argComputerPoints, argPlayerPoints){
+        if (argComputerPoints == 3) {
+            winner = 'Komputer wygrał grę!'
+            const elements = document.getElementsByClassName('cup');
+
+            elements[0].classList.add('cupWinner');
+
+            printWinner(winner);
+
+            showOverlay()
+            playAgain()
+
+        } else if (argPlayerPoints == 3) {
+            winner = 'Wygrałeś grę!'
+
+            const elements = document.getElementsByClassName('cup');
+
+            elements[1].classList.add('cupWinner');
+
+            printWinner(winner);
+
+            showOverlay()
+            playAgain()
+        }
+    }
+
+    function showOverlay() {
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = 'block';
+    }
+
+    function playAgain() {
+        let playerChoice = confirm('Czy chcesz zagrać jeszcze raz?');
+    
+        if (playerChoice) {
+            location.reload();
+        } else {
+            console.log('Gracz nie chce grać ponownie.');
+        }
+    }
+    
 
     // COMPUTER MOVE
 
@@ -46,29 +112,57 @@ function playGame(argPlayerMove) {
 
     // PLAYER MOVE
 
-    // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    // console.log('Gracz wpisał: ' + playerInput);
-
     let playerMove = getMoveName(argPlayerMove);
 
     // RESULT
 
     displayResult(computerMove, playerMove)
+
+    // RESULT TABLE
+    let resultTable = 'Komputer ' + computerPoints + ' - ' + playerPoints + ' Gracz';
+    printResultTable(resultTable)
+
+    // TELL WHO IS THE WINNER
+    whoWon(computerPoints, playerPoints);
 }
 
-// LISTENER
+// DECLARATION VARIABLES OF POINTS
+
+let computerPoints = 0;
+let playerPoints = 0;
+
+// DECLARATION VARIABLE WHO WON
+
+let winner = '';
+
+// LISTENERS
 
 document.getElementById('play-rock').addEventListener('click', function()
 {
+    document.getElementById('play-scissors').classList.remove('choice');
+    document.getElementById('play-paper').classList.remove('choice');
+
+    this.classList.add('choice');
+
     playGame(1);
 });
 
 document.getElementById('play-paper').addEventListener('click', function()
 {
+    document.getElementById('play-rock').classList.remove('choice');
+    document.getElementById('play-scissors').classList.remove('choice');
+
+    this.classList.add('choice');
+
     playGame(2);
 });
 
 document.getElementById('play-scissors').addEventListener('click', function()
 {
+    document.getElementById('play-rock').classList.remove('choice');
+    document.getElementById('play-paper').classList.remove('choice');
+    
+    this.classList.add('choice');
+
     playGame(3);
-});
+})
