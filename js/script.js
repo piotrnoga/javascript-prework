e3de5da46d7d61904a1a06f24a72b9761e6b72ad
Piotr Noga <piotrnoga.dev@gmail.com>
2024-08-25 15:45:07 +0200

    Test random number 11-19

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 
 let playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let randomNumber = Math.floor(Math.random() * 9 + 11);
+printMessage('Wylosowana liczba to: ' + randomNumber);
